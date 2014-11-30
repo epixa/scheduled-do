4e65bba417fe33fa070eeb0bd167104a041fda0d
Court Ewing <court@epixa.com>
2014-11-30 13:20:28 -0500

    Database trait now contains an accessor
    Attempting to access the db object before it has been set results in an
    exception being thrown.

--- lib/Database/DatabaseTrait.php
+++ lib/Database/DatabaseTrait.php
@@ -2,12 +2,35 @@
 
 namespace SDO\Database;
 
-use PDO;
-
-trait DatabaseTrait {
+/**
+ * Get/Set PDO database objects
+ */
+trait DatabaseTrait
+{
+  /**
+   * @var \PDO
+   */
   protected $db;
 
-  public function setDatabase(PDO $db) {
+  /**
+   * Sets the current database
+   * 
+   * @param \PDO $db
+   */
+  public function setDatabase(\PDO $db) {
     $this->db = $db;
+  }
+
+  /**
+   * Gets the current database
+   * 
+   * @throws \LogicException if no database has been set
+   * @return \PDO
+   */
+  public function getDatabase() {
+    if (!$this->db) {
+      throw new \LogicException('No database set, you must set a PDO instance via setDatabase() first');
+    }
+    return $this->db;
   }
 }
