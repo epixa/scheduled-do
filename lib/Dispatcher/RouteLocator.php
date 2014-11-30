bddfc7b5204d97a09f9afec3ef0adb8b64c29f9a
Court Ewing <court@epixa.com>
2014-11-30 18:16:21 -0500

    Comments for RouteLocator

--- lib/Dispatcher/RouteLocator.php
+++ lib/Dispatcher/RouteLocator.php
@@ -2,17 +2,45 @@
 
 namespace SDO\Dispatcher;
 
-class RouteLocator {
+use Interop\Container\ContainerInterface;
+
+/**
+ * Locates route controllers in container
+ */
+class RouteLocator
+{
   protected $container;
   protected $request;
   protected $response;
 
-  public function __construct($container, $request, $response) {
+  /**
+   * Constructs a container-aware route locator
+   *
+   * @param \Interop\Container\ContainerInterface $container
+   * @param object $request
+   * @param object $response
+   */
+  public function __construct(ContainerInterface $container, $request, $response) {
     $this->container = $container;
     $this->request = $request;
     $this->response = $response;
   }
 
+  /**
+   * Creates a routing function that wraps a controller action
+   *
+   * The given action name is parsed into a controller object, which is
+   * retrieved from the configured container, and a method name that is invoked
+   * on that controller.
+   *
+   * The configured values for request and response are passed as the last two
+   * arguments to each action.
+   *
+   * @see RouteLocator#assertValidName
+   *
+   * @param string $name Action name in the format <controller>@<action>
+   * @return callable
+   */
   public function route($name) {
     $this->assertValidName($name);
 
@@ -26,10 +54,21 @@ class RouteLocator {
     };
   }
 
+  /**
+   * Proxies invocation to route()
+   *
+   * @see RouteLocator#route
+   */
   public function __invoke($name) {
     return $this->route($name);
   }
 
+
+  /**
+   * Asserts that name is in the format <controller>@<action>
+   *
+   * @throws \InvalidArgumentException if route definition is invalid
+   */
   protected function assertValidName($name) {
     if (strpos($name, '@') === false) {
       throw new \InvalidArgumentException('Route definition must be in format <controller>@<action>');
