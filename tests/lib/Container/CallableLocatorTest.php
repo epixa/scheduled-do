7b8a5fb0e331d51954cdb3e1a02113fa0a49c3f0
Court Ewing <court@epixa.com>
2014-11-30 14:36:51 -0500

    Test for CallableLocator

---
+++ tests/lib/Container/CallableLocatorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SDO\Container;
+
+class CallableLocatorTest extends \PHPUnit_Framework_TestCase
+{
+  protected $locator;
+  protected $tmp;
+
+  protected function setUp() {
+    $container = $this->getMock('Interop\\Container\\ContainerInterface');
+
+    // all retrieved functions from container should return their own name
+    // so we can ensure the container is accessed with the given input
+    $container->method('get')
+              ->will($this->returnCallback(function($arg) {
+                return function() use ($arg) {
+                  return $arg;
+                };
+              }));
+
+    $this->locator = new CallableLocator($container);
+  }
+
+  public function testGetCallableFnWithFunction() {
+    $input = function() {};
+    $fn = $this->locator->getCallableFn($input);
+    $this->assertSame($input, $fn);
+  }
+
+  public function testGetCallableFnWithName() {
+    $fn = $this->locator->getCallableFn('foo');
+    $this->assertEquals($fn(), 'foo');
+  }
+
+  public function testInvokeWithFunction() {
+    $this->testGetCallableFnWithFunction();
+  }
+
+  public function testInvokeWithName() {
+    $this->testGetCallableFnWithName();
+  }
+
+  /**
+   * @expectedException InvalidArgumentException
+   */
+  public function testGetCallableFnWithInvalidFn() {
+    $this->locator->getCallableFn(7);
+  }
+}
