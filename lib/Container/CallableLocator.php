4821166058c2ff81fcf6a0d1b507eb519650ce08
Court Ewing <court@epixa.com>
2014-11-30 14:03:48 -0500

    CallableLocator is now container-aware
    The container is typehinted to ensure a real container is being passed
    rather than relying on ducktyping at call-time.

--- lib/Container/CallableLocator.php
+++ lib/Container/CallableLocator.php
@@ -2,14 +2,35 @@
 
 namespace SDO\Container;
 
+use Interop\Container\ContainerInterface;
+
+/**
+ * Locates callable objects in container
+ */
 class CallableLocator
 {
   protected $container;
 
-  public function __construct($container) {
+  /**
+   * Constructs a container-aware locator
+   * 
+   * @param \Interop\Container\ContainerInterface $container
+   */
+  public function __construct(ContainerInterface $container) {
     $this->container = $container;
   }
 
+  /**
+   * Locates and returns the given callable object
+   * 
+   * If a string is given, the function identified by that name is retrieved
+   * from the container. If a callable object is given, then it is returned
+   * without interference.
+   * 
+   * @param string|callable $fn
+   * @throws \InvalidArgumentException if the function is not callable
+   * @return callable
+   */
   public function getCallableFn($fn) {
     if (is_string($fn)) {
       $fn = $this->container->get($fn);
@@ -20,6 +41,11 @@ class CallableLocator
     return $fn;
   }
 
+  /**
+   * Proxies invocation to getCallableFn()
+   * 
+   * @see CallableLocator#getCallableFn
+   */
   public function __invoke($name) {
     return $this->getCallableFn($name);
   }
