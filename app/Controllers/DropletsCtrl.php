34a552bd5936e3401c7846ff982f81b2f8f816c4
Court Ewing <court@epixa.com>
2014-12-01 15:09:08 -0500

    Comment droplets controller

--- app/Controllers/DropletsCtrl.php
+++ app/Controllers/DropletsCtrl.php
@@ -5,14 +5,26 @@ namespace App\Controllers;
 use SDO\Response\SendableTrait;
 use App\Traits\DropletsTrait;
 
-class DropletsCtrl {
+/**
+ * Control request/response processing for droplet actions
+ */
+class DropletsCtrl
+{
   use SendableTrait;
   use DropletsTrait;
 
+  /**
+   * Gets all droplets
+   */
   public function index() {
     $this->send(['droplets' => $this->droplets->all()]);
   }
 
+  /**
+   * Gets a specific droplet based on the given id
+   *
+   * @param int $id
+   */
   public function get($id) {
     $droplet = $this->droplets->one($id);
     if (!$droplet) return $this->send(404);
