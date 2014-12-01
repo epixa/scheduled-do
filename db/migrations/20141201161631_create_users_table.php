84561aab2cb98ae66b333ffd4c720718d21ebb55
Court Ewing <court@epixa.com>
2014-12-01 15:13:57 -0500

    Tweak structure of migrations
    There are no programmatic changes to the migrations here, as that would
    defeat the purpose of migrations. This is simply a style cleanup of the
    automatically generated classes.

--- db/migrations/20141201161631_create_users_table.php
+++ db/migrations/20141201161631_create_users_table.php
@@ -4,14 +4,6 @@ use Phinx\Migration\AbstractMigration;
 
 class CreateUsersTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     */
     public function change()
     {
         $table = $this->table('users');
@@ -22,19 +14,6 @@ class CreateUsersTable extends AbstractMigration
               ->create();
     }
     
-    /**
-     * Migrate Up.
-     */
-    public function up()
-    {
-    
-    }
-
-    /**
-     * Migrate Down.
-     */
-    public function down()
-    {
-
-    }
+    public function up() {}
+    public function down() {}
 }
--- db/migrations/20141201161632_create_droplets_table.php
+++ db/migrations/20141201161632_create_droplets_table.php
@@ -4,14 +4,6 @@ use Phinx\Migration\AbstractMigration;
 
 class CreateDropletsTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     */
     public function change()
     {
         $table = $this->table('droplets');
@@ -29,39 +21,6 @@ class CreateDropletsTable extends AbstractMigration
               ->create();
     }
     
-    /**
-     * Migrate Up.
-     */
-    public function up()
-    {
-    
-    }
-
-    /**
-     * Migrate Down.
-     */
-    public function down()
-    {
-
-    }
+    public function up() {}
+    public function down() {}
 }
-/*
-| droplets | CREATE TABLE `droplets` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `do_id` int(10) unsigned NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `region` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `memory` int(10) unsigned NOT NULL,
-  `vcpus` int(10) unsigned NOT NULL,
-  `disk` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `droplets_do_id_unique` (`do_id`),
-  KEY `droplets_user_id_foreign` (`user_id`),
-  CONSTRAINT `droplets_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci |
-*/
